04dfa9867b0e6a998d8703b66f1561f8668bc92a
Björn Feber <bfeber@protonmail.com>
2024-01-27 17:22:19 +0100

    Add initial intro level layout

---
+++ SimShock/Scripts/Wieldable_Hammer.gd
@@ -0,0 +1,20 @@
+extends Node3D
+
+
+@export var default_position : Vector3
+
+@export_group("Audio")
+@export var sound_primary_use : AudioStream
+@export var sound_secondary_use : AudioStream
+
+# Stores the player interaction component
+var player_interaction_component
+
+
+# This gets called by player interaction compoment when the wieldable is equipped and primary action is pressed
+func action_primary(_camera_collision:Vector3, _passed_item_reference : InventoryItemPD):
+	print("Hammer.gd: action_primary called. Self: ", self)
+
+
+func action_secondary(is_released:bool):
+	print("Hammer.gd: action_secondary called. Self: ", self)
