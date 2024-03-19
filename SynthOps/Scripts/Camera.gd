7bbf6c9885a8dc31ddd058912fbf129b527887e8
Björn Feber <bfeber@protonmail.com>
2024-03-19 18:01:56 +0100

    Improved camera

--- SynthOps/Scripts/Camera.gd
+++ SynthOps/Scripts/Camera.gd
@@ -1,7 +1,6 @@
 extends Node3D
 
 
-@export var player: CharacterBody3D
 @export var cone_material_default: StandardMaterial3D
 @export var cone_material_detected: StandardMaterial3D
 
@@ -9,7 +8,20 @@ extends Node3D
 @onready var rotation_point: Node3D = $RotationPoint
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-var currently_detecting: bool = false
+var threat: CharacterBody3D
+var is_detecting: bool:
+	set(value):
+		if is_detecting != value:
+			is_detecting = value
+
+			if value:
+				threat_detected.emit()
+				animation_player.pause()
+				$RotationPoint/CSGCylinder3D.material = cone_material_detected
+			else:
+				threat_lost.emit()
+				animation_player.play()
+				$RotationPoint/CSGCylinder3D.material = cone_material_default
 
 signal threat_detected()
 signal threat_lost()
@@ -17,19 +29,25 @@ signal threat_lost()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var body := raycast.get_collider() as CharacterBody3D
-	if body and body.is_in_group("Player"):
-		if not currently_detecting:
-			threat_detected.emit()
-			currently_detecting = true
-			animation_player.pause()
-			$RotationPoint/CSGCylinder3D2.material = cone_material_detected
-	else:
-		$RotationPoint/CSGCylinder3D2.height = raycast.get_collision_point().distance_to(rotation_point.global_position)
-		$RotationPoint/CSGCylinder3D2.position.z = -$RotationPoint/CSGCylinder3D2.height / 2 - 0.5
+	if threat:
+		raycast.look_at(threat.global_position)
+		raycast.force_raycast_update()
 
-		if currently_detecting:
-			threat_lost.emit()
-			currently_detecting = false
-			animation_player.play()
-			$RotationPoint/CSGCylinder3D2.material = cone_material_default
+		if raycast.get_collider() is Player:
+			is_detecting = true
+		else:
+			is_detecting = false
+	else:
+		is_detecting = false
+
+
+func _on_area_3d_body_entered(body):
+	var player := body as Player
+	if player:
+		threat = player
+
+
+func _on_area_3d_body_exited(body):
+	var player := body as Player
+	if player:
+		threat = null
